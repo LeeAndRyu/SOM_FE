da8aa8ae6eaa3a834a27ffe327a8ef85d2a08334
oluzr <olu-zr@naver.com>
2024-01-18 14:32:05 +0900

    :white_check_mark: Add : react-hook-form 라이브러리 설치
    form 라이브러리 설치

--- src/components/auth/signUpModal.tsx
+++ src/components/auth/signUpModal.tsx
@@ -1,12 +1,21 @@
 import { useEffect, useState } from 'react'
 import clsx from 'clsx'
+import { useForm } from 'react-hook-form'
 const SignUpModal = () => {
   const [isCorrect, setCorrect] = useState(false)
+  const [isTyping, setTyping] = useState(false)
+  const { register, getValues, handleSubmit } = useForm()
+  const onSubmitHandler = (e: any) => {}
+  const onChangeHandler = () => {
+    setTyping(true)
+  }
   useEffect(() => {
-    
-  },[])
+    const pwdValue = getValues('password')
+    console.log(pwdValue)
+    console.log()
+  }, [])
   return (
-    <form className='card-body'>
+    <form className='card-body' onSubmit={handleSubmit(onSubmitHandler)}>
       <h2 className='text-center text-xl'>회원가입</h2>
       <div className='form-control'>
         <label className='label'>
@@ -15,6 +24,7 @@ const SignUpModal = () => {
         <input
           type='text'
           placeholder='name'
+          {...register('name')}
           className='input input-bordered'
           required
         />
@@ -26,6 +36,7 @@ const SignUpModal = () => {
         <input
           type='email'
           placeholder='email'
+          {...register('email')}
           className='input input-bordered'
           required
         />
@@ -37,6 +48,7 @@ const SignUpModal = () => {
         <input
           type='password'
           placeholder='password'
+          {...register('password')}
           className='input input-bordered mb-1.5 info-success'
           required
         />
@@ -50,7 +62,7 @@ const SignUpModal = () => {
           placeholder='confirm password'
           className={clsx(
             `input input-bordered mb-1.5`,
-            isCorrect ? 'input-success' : 'input-error'
+            isTyping ? (isCorrect ? 'input-success' : 'input-error') : ''
           )}
           // required
         />
