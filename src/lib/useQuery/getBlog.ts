2a4fd9a07540a4af8fc6707c3c10ee5232a7f711
oluzr <olu-zr@naver.com>
2024-01-29 17:37:29 +0900

    :white_check_mark: Update : 글 post react-quill
    api 연동 작업

--- src/lib/useQuery/getBlog.ts
+++
@@ -1,8 +0,0 @@
-import { axiosInstance } from '../axios'
-export function getBlog({ queryKey }: any) {
-  const [_1, accountName] = queryKey
-  const res = axiosInstance
-    .get(`/blog/${accountName}/member`)
-    .then((res) => res.data)
-  return res
-}
--- src/types/app.ts
+++
@@ -1,1 +0,0 @@
-export type LoginModalType = 'login' | 'findPwd' | 'signUp'
